c1be91e5bf5fbbda0f034a7df7628bbaebbb26d3
SCsupercraft <103709272+SCsupercraft@users.noreply.github.com>
2025-06-23 15:44:13 +0100

    Add texture for ender creeper spawn egg

--- src/main/java/net/brocker/monsterbreeder/datagen/ModModelProvider.java
+++ src/main/java/net/brocker/monsterbreeder/datagen/ModModelProvider.java
@@ -1,10 +1,14 @@
 package net.brocker.monsterbreeder.datagen;
 
-import net.brocker.monsterbreeder.block.ModBlocks;
+import net.brocker.monsterbreeder.item.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
 import net.minecraft.data.client.BlockStateModelGenerator;
 import net.minecraft.data.client.ItemModelGenerator;
+import net.minecraft.data.client.Model;
+import net.minecraft.util.Identifier;
+
+import java.util.Optional;
 
 public class ModModelProvider extends FabricModelProvider {
     public ModModelProvider(FabricDataOutput output) {
@@ -14,12 +18,14 @@ public class ModModelProvider extends FabricModelProvider {
     @Override
     public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
         // blockStateModelGenerator.registerNorthDefaultHorizontalRotation(ModBlocks.DNA_ALTAR);
-        //blockStateModelGenerator.registerItemModel(ModBlocks.DNA_ALTAR);
-        //blockStateModelGenerator.registerItemModel(ModBlocks.BIO_REACTION_CHAMBER);
+        // blockStateModelGenerator.registerItemModel(ModBlocks.DNA_ALTAR);
+        // blockStateModelGenerator.registerItemModel(ModBlocks.BIO_REACTION_CHAMBER);
     }
 
     @Override
     public void generateItemModels(ItemModelGenerator itemModelGenerator) {
+        Model spawnEggModel = new Model(Optional.of(Identifier.of("item/template_spawn_egg")), Optional.empty());
 
+        itemModelGenerator.register(ModItems.ENDER_CREEPER_SPAWN_EGG, spawnEggModel);
     }
 }
